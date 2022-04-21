74c0dc78a968f8cbb2e5e5a39ba4249cbf1142d0
胥永杰 <99706807@qq.com>
2022-04-21 08:03:09 +0800

    登录

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,51 @@
-import { Button } from "antd";
+import logo from "../../assets/logo.png";
+import "./index.sass";
+import { Form, Input, Button, Checkbox } from "antd";
 const Login = () => {
-  return <Button type="primary">Button</Button>;
+  return (
+    <div className="login">
+      <div className="login-container">
+        <img className="login-logo" src={logo} alt=""></img>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          autoComplete="off"
+          size="large"
+        >
+          <Form.Item
+            label="帐号"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Checkbox>我已阅读并同意「用户协议」和「隐私条款」</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 export default Login;
