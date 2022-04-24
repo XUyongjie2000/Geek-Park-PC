f733b195972b4761af2376fde2c1a8e4cc9f45a9
胥永杰 <99706807@qq.com>
2022-04-24 08:29:33 +0800

    请求头

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,7 +4,7 @@ const http = axios.create({
   baseURL: "http://geek.itheima.net/v1_0",
   timeout: 5000,
 });
-//同意添加请求头
+//统一添加请求头
 http.interceptors.response.use(
   (res) => {
     return res?.data?.data || res;
